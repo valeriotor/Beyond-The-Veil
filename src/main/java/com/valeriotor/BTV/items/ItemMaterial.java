e0e6deca0b46a802866eedc7677561012c967247
Unknown <valeriotorsiello@libero.it>
2019-05-21 21:46:53 +0200

    Added ItemMaterial class for generic materials such as the Dorsal Spine.

---
+++ src/main/java/com/valeriotor/BTV/items/ItemMaterial.java
@@ -0,0 +1,30 @@
+package com.valeriotor.BTV.items;
+
+import java.util.List;
+
+import com.valeriotor.BTV.lib.References;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemMaterial extends Item{
+	
+	public ItemMaterial(String name) {
+		this.setRegistryName(References.MODID, name);
+		this.setUnlocalizedName(name);
+		this.setCreativeTab(References.BTV_TAB);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		String info = I18n.format(String.format("lore.%s",  this.getUnlocalizedName().substring(5)));
+		if(!info.substring(0,4).equals("lore")) tooltip.add("§5§o" + info);
+	}
+	
+}
