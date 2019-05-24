e5d49ee3021c102c42b71bc6581100258b2f5b14
Valerio Torsiello <valeriotorsiello@libero.it>
2019-05-24 21:37:24 +0200

    GuiWateryCradle now takes a BlockPos parameter for the Cradle's position

--- src/main/java/com/valeriotor/BTV/gui/GuiWateryCradle.java
+++ src/main/java/com/valeriotor/BTV/gui/GuiWateryCradle.java
@@ -3,9 +3,28 @@ package com.valeriotor.BTV.gui;
 import com.valeriotor.BTV.lib.References;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class GuiWateryCradle extends GuiOptionWheel{
 
+	private final int[] coords = new int[3];
+	
+	
+	public GuiWateryCradle(BlockPos pos) {
+		this.coords[0] = pos.getX();
+		this.coords[1] = pos.getY();
+		this.coords[2] = pos.getZ();
+	}
+	
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		System.out.print(this.coords[0]);
+		super.drawScreen(mouseX, mouseY, partialTicks);
+	}
+	
 	@Override
 	public String getGuiLangName() {
 		return "guiWateryCradle";
