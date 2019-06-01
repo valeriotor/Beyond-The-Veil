4b0b0f54b61b20a30ec91eaf7351b1597f5d000d
Unknown <valeriotorsiello@libero.it>
2019-06-01 14:04:00 +0200

    Changed TilePlantTerra cooldown.

--- src/main/java/com/valeriotor/BTV/tileEntities/TilePlantTerra.java
+++ src/main/java/com/valeriotor/BTV/tileEntities/TilePlantTerra.java
@@ -15,7 +15,7 @@ public class TilePlantTerra extends TileEntity implements ITickable{
 	public void update() {
 		if(this.world.getBlockState(pos.down()).getBlock() != Blocks.AIR && this.world.getBlockState(this.pos.down()).getBlockHardness(this.world, this.pos.up()) >= 0) this.counter++;
 		else this.counter = 0;
-		if(this.counter >= 5000) {
+		if(this.counter >= 50) {
 			this.counter = 0;
 			this.world.destroyBlock(this.pos.down(), true);
 		}
