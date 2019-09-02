50667d0a816d4c6b1949905d3fb89e6499c5c5ec
Valerio Torsiello <valeriotorsiello@libero.it>
2019-09-02 22:56:07 +0200

    Dark Sand now has a different MapColor (that of blue hardened clay).

--- src/main/java/com/valeriotor/BTV/blocks/BlockDarkSand.java
+++ src/main/java/com/valeriotor/BTV/blocks/BlockDarkSand.java
@@ -8,9 +8,13 @@ import com.valeriotor.BTV.lib.References;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFalling;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockDarkSand extends BlockFalling{
 
@@ -24,7 +28,10 @@ public class BlockDarkSand extends BlockFalling{
 		this.setSoundType(SoundType.SAND);
 	}
 	
-	
+	@Override
+	public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+		return MapColor.BLUE_STAINED_HARDENED_CLAY;
+	}
 	
 	
 	
