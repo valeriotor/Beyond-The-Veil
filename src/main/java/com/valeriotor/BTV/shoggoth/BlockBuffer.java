15bee4bf9604cdca5ff459b91dca6c6af6d54837
Valerio Torsiello <valeriotorsiello@libero.it>
2019-09-22 16:14:52 +0200

    BlockBuffer can now be written to and read from NBT.
    Will be used for custom Buildings.
    
    Currently untested, 90% chance of a bug somewhere.

--- src/main/java/com/valeriotor/BTV/shoggoth/BlockBuffer.java
+++ src/main/java/com/valeriotor/BTV/shoggoth/BlockBuffer.java
@@ -1,11 +1,16 @@
 package com.valeriotor.BTV.shoggoth;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
+
+import com.valeriotor.BTV.blocks.BlockRegistry;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Rotation;
 
 public class BlockBuffer {
@@ -45,6 +50,32 @@ public class BlockBuffer {
 		this.zSize = (short) zSize;
 	}
 	
+	public BlockBuffer(NBTTagCompound nbt) {
+		this.xSize = nbt.getShort("xSize");
+		this.ySize = nbt.getShort("ySize");
+		this.zSize = nbt.getShort("zSize");
+		this.coords = new short[xSize][ySize][zSize];
+		this.metadata = new byte[xSize][ySize][zSize];
+		int i = 0;
+		List<Block> blocks = new ArrayList<>();
+		while(nbt.hasKey(String.format("b%d", i))) blocks.add(Block.getBlockFromName(nbt.getString(String.format("b%d", i++)))); 
+		this.blocks = (Block[]) blocks.toArray();
+		int x = 0, y, z;
+		NBTTagCompound xnbt, ynbt;
+		while(nbt.hasKey(String.format("x%d", x))) {
+			y = 0;
+			xnbt = nbt.getCompoundTag(String.format("x%d", x));
+			while(xnbt.hasKey(String.format("y%d", y))) {
+				z = 0;
+				ynbt = nbt.getCompoundTag("y" + y);
+				while(ynbt.hasKey("s" + z)) {
+					this.coords[x][y][z] = ynbt.getShort("s" + z);
+					this.metadata[x][y][z] = ynbt.getByte("b" + z);
+				}
+			}
+		}
+	}
+	
 	public int getX(int progress) {
 		return (progress % xSize) - xSize / 2;
 	}
@@ -69,6 +100,33 @@ public class BlockBuffer {
 	public IBlockState getRotatedBlockState(int x, int y, int z, int rotation) {
 		IBlockState state = this.getBlockState(x, y, z).withRotation(Rotation.values()[rotation]);
 		return state;
+	}
+	
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+		NBTTagCompound xnbt;
+		NBTTagCompound ynbt;
+		for(int x = 0; x < this.xSize; x++) {
+			xnbt = new NBTTagCompound();
+			for(int y = 0; y < this.ySize; y++) {
+				ynbt = new NBTTagCompound();
+				for(int z = 0; z < this.zSize; z++) {
+					ynbt.setShort(String.format("s%d", z), this.coords[x][y][z]);
+					ynbt.setByte(String.format("b%d", z), this.metadata[x][y][z]);
+				}
+				xnbt.setTag(String.format("y%d", y), ynbt);
+			}
+			nbt.setTag(String.format("x%d", x), xnbt);
+		}
+		
+		NBTTagCompound blocks = new NBTTagCompound();
+		for(int i = 0; i < this.blocks.length; i++) {
+			blocks.setString(String.format("b%d", i), this.blocks[i].getRegistryName().toString());
+		}
+		nbt.setTag("block", blocks);
+		nbt.setInteger("xSize", xSize);
+		nbt.setInteger("ySize", ySize);
+		nbt.setInteger("zSize", zSize);
+		return nbt;
 	}
 	
 }
