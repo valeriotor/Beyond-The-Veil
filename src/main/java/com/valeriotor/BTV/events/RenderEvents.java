d2f9b4f0f33112ac5f2bafa63750a7c0882b2462
Valerio Torsiello <valeriotorsiello@libero.it>
2019-09-22 16:27:30 +0200

    Added method to display Player names under specific conditions.
    Will be used for teleportation.

--- src/main/java/com/valeriotor/BTV/events/RenderEvents.java
+++ src/main/java/com/valeriotor/BTV/events/RenderEvents.java
@@ -2,6 +2,7 @@ package com.valeriotor.BTV.events;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import org.lwjgl.opengl.GL11;
 
@@ -9,12 +10,14 @@ import com.valeriotor.BTV.entities.render.RenderTransformedPlayer;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -24,8 +27,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class RenderEvents {
 	
 	public final Set<EntityPlayer> transformedPlayers = new HashSet();
+	private final Set<UUID> covenantPlayers = new HashSet<>();
 	private static final RenderTransformedPlayer deepOne = new RenderTransformedPlayer(Minecraft.getMinecraft().getRenderManager());
-	
 	
 	@SubscribeEvent
 	public void onPlayerRenderEvent(RenderPlayerEvent.Pre event) {
@@ -54,6 +57,30 @@ public class RenderEvents {
 			Minecraft.getMinecraft().player.eyeHeight = 2;
 		else
 			Minecraft.getMinecraft().player.eyeHeight = 1.62F;
+	}
+	
+	@SubscribeEvent
+	public void onOverlayRenderEvent(RenderWorldLastEvent event) {
+		if(Minecraft.getMinecraft().player == null || Minecraft.getMinecraft().isGamePaused() || Minecraft.getMinecraft().player.world == null) return;
+		renderCovenantPlayers(event);
+	}
+	
+	public void renderCovenantPlayers(RenderWorldLastEvent event) {
+		if(covenantPlayers.isEmpty()) return;
+		EntityPlayer p = Minecraft.getMinecraft().player;
+		RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
+		double d0 = p.lastTickPosX + (p.posX - p.lastTickPosX) * (double)event.getPartialTicks();
+        double d1 = p.lastTickPosY + (p.posY - p.lastTickPosY) * (double)event.getPartialTicks();
+        double d2 = p.lastTickPosZ + (p.posZ - p.lastTickPosZ) * (double)event.getPartialTicks();
+        float f = renderManager.playerViewY;
+        float f1 = renderManager.playerViewX;
+        boolean flag1 = renderManager.options.thirdPersonView == 2;
+		for(UUID u : covenantPlayers) {
+			EntityPlayer e = p.world.getPlayerEntityByUUID(u);
+			if(e == null) continue;
+			float dist = (float)Math.sqrt(e.getDistanceSq(p));
+            EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, e.getName(), 10*(float)(e.posX - d0)/dist, 10*(float)(e.posY - d1)/dist, 10*(float)(e.posZ - d2)/dist, 0, f, f1, flag1, false);
+        }
 	}
 	
 }
