454780f398a00f41d0fc60d5b07e1c8ba8702550
Unknown <valeriotorsiello@libero.it>
2019-10-06 21:47:08 +0200

    Shoggoths no longer suffocate. They now also have a damage cap.

--- src/main/java/com/valeriotor/BTV/events/LivingEvents.java
+++ src/main/java/com/valeriotor/BTV/events/LivingEvents.java
@@ -1,13 +1,12 @@
 package com.valeriotor.BTV.events;
 
 import com.valeriotor.BTV.entities.EntityFletum;
-import com.valeriotor.BTV.worship.DGWorshipHelper;
-import com.valeriotor.BTV.worship.Deities;
+import com.valeriotor.BTV.entities.EntityShoggoth;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
-import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,18 +14,32 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class LivingEvents {
 	
+	private static final boolean DEBUG = true;
+	
 	@SubscribeEvent
 	public static void livingHurt(LivingHurtEvent e) {
-		if(e.getEntity().world.isRemote) return;
 		if(e.getSource() instanceof EntityDamageSource) {
 			if(e.getSource().getTrueSource() instanceof EntityPlayer) {
 				GreatDreamerBuffs.applyAttackModifier(e);
 			}
-		}else if(e.getSource() == DamageSource.FALL) {
-			if(e.getEntity() instanceof EntityFletum) {
-				e.setCanceled(true);
-			}
 		}
+		e.setCanceled(cancelDamage(e));
+	}
+	
+	@SuppressWarnings("unused")
+	public static boolean cancelDamage(LivingHurtEvent e) {
+		EntityLivingBase ent = e.getEntityLiving();
+		DamageSource d = e.getSource();
+		if(ent instanceof EntityFletum) {
+			if(d == DamageSource.FALL) return true;
+		} else if(ent instanceof EntityShoggoth) {
+			if( d == DamageSource.FALL ||
+				d == DamageSource.IN_WALL || 
+				d == DamageSource.CRAMMING) return true;
+			if(e.getAmount() > 8 && !DEBUG) e.setAmount(8);
+		}
+		
+		return false;
 	}
 	
 }
