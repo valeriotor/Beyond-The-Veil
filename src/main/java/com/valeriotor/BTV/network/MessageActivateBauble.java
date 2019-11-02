a73df08d0f0b6a0fb591a0459dbf85f847ad708d
Unknown <valeriotorsiello@libero.it>
2019-11-02 01:25:23 +0100

    Failed Bauble Activation now tells you the remaining cooldown.

--- src/main/java/com/valeriotor/BTV/network/MessageActivateBauble.java
+++ src/main/java/com/valeriotor/BTV/network/MessageActivateBauble.java
@@ -1,9 +1,9 @@
 package com.valeriotor.BTV.network;
 
 import com.valeriotor.BTV.animations.AnimationRegistry;
+import com.valeriotor.BTV.capabilities.IPlayerData;
 import com.valeriotor.BTV.capabilities.PlayerDataProvider;
 import com.valeriotor.BTV.entities.BTVEntityRegistry;
-import com.valeriotor.BTV.entities.EntityDeepOne;
 import com.valeriotor.BTV.items.baubles.IActiveBauble;
 import com.valeriotor.BTV.lib.BTVSounds;
 import com.valeriotor.BTV.lib.PlayerDataLib;
@@ -17,6 +17,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -38,7 +39,8 @@ public class MessageActivateBauble implements IMessage{
 		@Override
 		public IMessage onMessage(MessageActivateBauble message, MessageContext ctx) {
 			EntityPlayer p = ctx.getServerHandler().player;
-			if(p.getCapability(PlayerDataProvider.PLAYERDATA, null).getString(PlayerDataLib.TRANSFORMED)) {
+			IPlayerData data = p.getCapability(PlayerDataProvider.PLAYERDATA, null);
+			if(data.getString(PlayerDataLib.TRANSFORMED)) {
 				p.world.getEntities(EntityLivingBase.class, e -> e.getDistance(p) < 25)
 				 .forEach(e -> {
 				 if(e != p && !BTVEntityRegistry.isFearlessEntity(e)) {
@@ -51,14 +53,16 @@ public class MessageActivateBauble implements IMessage{
 				 });
 				BTVPacketHandler.INSTANCE.sendToAll(new MessagePlayerAnimation(p.getPersistentID(), AnimationRegistry.getIdFromAnimation(AnimationRegistry.deep_one_roar)));
 			}else {
-				int selected = p.getCapability(PlayerDataProvider.PLAYERDATA, null).getOrSetInteger(PlayerDataLib.SELECTED_BAUBLE, -1, false);
+				int selected = data.getOrSetInteger(PlayerDataLib.SELECTED_BAUBLE, -1, false);
 				if(selected == -1) return null;
 				ItemStack stack = BaublesApi.getBaublesHandler(p).getStackInSlot(selected);
 				if(stack.getItem() instanceof IActiveBauble) {
 					String key = String.format(PlayerDataLib.BAUBLE_COOLDOWN, selected);
-					if(p.getCapability(PlayerDataProvider.PLAYERDATA, null).getOrSetInteger(key, 0, false) > 0) return null; //Maybe add audio clue?
-					if(((IActiveBauble)stack.getItem()).activate(p))
-						p.getCapability(PlayerDataProvider.PLAYERDATA, null).setInteger(key, ((IActiveBauble)stack.getItem()).getCooldown(), false);
+					int cooldown = data.getOrSetInteger(key, 0, false);
+					if(cooldown > 0) 
+						p.sendMessage(new TextComponentTranslation("bauble.cooldown", cooldown/20));
+					else if(((IActiveBauble)stack.getItem()).activate(p))
+						data.setInteger(key, ((IActiveBauble)stack.getItem()).getCooldown(), false);
 				}
 			}
 			return null;
