d1f73a6785e6ca280c207ecfa9239d80b9183f8b
Unknown <valeriotorsiello@libero.it>
2019-11-08 19:54:22 +0100

    PlayerTimers can now have multiple continuos and final actions.

--- src/main/java/com/valeriotor/BTV/util/PlayerTimer.java
+++ src/main/java/com/valeriotor/BTV/util/PlayerTimer.java
@@ -1,35 +1,49 @@
 package com.valeriotor.BTV.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+
+import com.google.common.collect.ImmutableList;
 
 import net.minecraft.entity.player.EntityPlayer;
 
 public class PlayerTimer {
 	public final EntityPlayer player;
 	private int timer;
-	private final Consumer<EntityPlayer> action;
+	private final List<Consumer<EntityPlayer>> finalActions;
+	private final List<Consumer<EntityPlayer>> continuosActions;
 	
 	public PlayerTimer(EntityPlayer player) {
 		this(player, null, 100);
 	}
 	
-	public PlayerTimer(EntityPlayer player, Consumer<EntityPlayer> action) {
-		this(player, null, 100);
+	public PlayerTimer(EntityPlayer player, Consumer<EntityPlayer> finalAction) {
+		this(player, finalAction, 100);
 	}
 	
 	public PlayerTimer(EntityPlayer player, Consumer<EntityPlayer> action, int timer) {
 		this.player = player;
 		this.timer = timer;
-		this.action = action;
+		this.continuosActions = ImmutableList.of();
+		this.finalActions = ImmutableList.of(action);
+	}
+	
+	private PlayerTimer(EntityPlayer player, int timer, List<Consumer<EntityPlayer>> continuosActions, List<Consumer<EntityPlayer>> finalActions) {
+		this.player = player;
+		this.timer = timer;
+		this.continuosActions = ImmutableList.copyOf(continuosActions);
+		this.finalActions = ImmutableList.copyOf(finalActions);
 	}
 	
 	public boolean update() {
 		if(timer > 0) {
 			timer--;
+			for(Consumer<EntityPlayer> action : continuosActions) action.accept(player);
 			return false;
 		} else if(timer == 0) {
 			timer--;
-			if(action != null) action.accept(player);
+			for(Consumer<EntityPlayer> action : finalActions) action.accept(player);
 		}
 		return true;
 		
@@ -39,17 +53,35 @@ public class PlayerTimer {
 		return timer <= 0;
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		if(!(obj instanceof PlayerTimer)) return false;
-		PlayerTimer cov = (PlayerTimer)obj;
-		if(cov == this) return true;
-		if(cov.player.equals(this.player)) return true;
-		return false;
-	}
-	
-	@Override
-	public int hashCode() {
-		return player.hashCode();
+	public static class PlayerTimerBuilder {
+		
+		private List<Consumer<EntityPlayer>> finalActions = new ArrayList<>();
+		private List<Consumer<EntityPlayer>> continuosActions = new ArrayList<>();
+		private int timer = 100;
+		private final EntityPlayer player;
+		
+		public PlayerTimerBuilder(EntityPlayer player) {
+			this.player = player;
+		}
+		
+		public PlayerTimerBuilder setTimer(int timer) {
+			this.timer = timer;
+			return this;
+		}
+		
+		public PlayerTimerBuilder addFinalAction(Consumer<EntityPlayer> action) {
+			finalActions.add(action);
+			return this;
+		}
+		
+		public PlayerTimerBuilder addContinuosAction(Consumer<EntityPlayer> action) {
+			continuosActions.add(action);
+			return this;
+		}
+		
+		public PlayerTimer toPlayerTimer() {
+			return new PlayerTimer(this.player, this.timer, this.continuosActions, this.finalActions);
+		}
+		
 	}
 }
