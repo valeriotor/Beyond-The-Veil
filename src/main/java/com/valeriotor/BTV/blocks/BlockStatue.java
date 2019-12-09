be9ff4447564d4542cc34a29984c81f8dc144521
Unknown <valeriotorsiello@libero.it>
2019-12-09 20:20:40 +0100

    Added "facing" property to Statues.

--- src/main/java/com/valeriotor/BTV/blocks/BlockStatue.java
+++ src/main/java/com/valeriotor/BTV/blocks/BlockStatue.java
@@ -5,16 +5,24 @@ import com.valeriotor.BTV.worship.CrawlerWorship.WorshipType;
 
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import static com.valeriotor.BTV.blocks.BlockProperties.HORIZONTAL_FACING;;
 
 public class BlockStatue extends ModBlock implements ITileEntityProvider{
 	
@@ -25,6 +33,7 @@ public class BlockStatue extends ModBlock implements ITileEntityProvider{
 	public BlockStatue(Material material, String name, WorshipType type) {
 		super(material, name);
 		this.type = type;
+		this.setDefaultState(this.blockState.getBaseState().withProperty(HORIZONTAL_FACING, EnumFacing.SOUTH));
 	}
 
 	@Override
@@ -58,5 +67,56 @@ public class BlockStatue extends ModBlock implements ITileEntityProvider{
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
+	
+	@Override
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+		return this.getDefaultState().withProperty(HORIZONTAL_FACING, placer.getHorizontalFacing());
+    }
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		IBlockState state = this.getDefaultState();
+		switch(meta) {
+			case 0:
+				state = state.withProperty(HORIZONTAL_FACING, EnumFacing.SOUTH);
+				break;
+			case 1:
+				state = state.withProperty(HORIZONTAL_FACING, EnumFacing.WEST);
+				break;
+			case 2:
+				state = state.withProperty(HORIZONTAL_FACING, EnumFacing.NORTH);
+				break;
+			case 3:
+				state = state.withProperty(HORIZONTAL_FACING, EnumFacing.EAST);
+				break;	
+		}		
+		return state;
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(HORIZONTAL_FACING).getHorizontalIndex();
+	}
+	
+	public IBlockState withRotation(IBlockState state, Rotation rot)
+    {
+        return state.withProperty(HORIZONTAL_FACING, rot.rotate((EnumFacing)state.getValue(HORIZONTAL_FACING)));
+    }
+	
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
+    {
+        return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(HORIZONTAL_FACING)));
+    }
+	
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return BlockFaceShape.UNDEFINED;
+    }
+	
+	protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {HORIZONTAL_FACING});
+    }
 
 }
