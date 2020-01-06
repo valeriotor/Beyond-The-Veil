1f82a29b621df887eda0260c3cec47cbede2b30b
Valerio Torsiello <valeriotorsiello@libero.it>
2020-01-06 01:56:54 +0100

    Some renaming.

--- src/main/java/com/valeriotor/BTV/dreaming/DreamRegistry.java
+++ src/main/java/com/valeriotor/BTV/dreaming/DreamRegistry.java
@@ -24,42 +24,42 @@ import net.minecraft.init.Blocks;
 
 public class DreamRegistry {
 	
-	public static final DreamEldritch alienis = new DreamEldritch("alienis", 3);
-	public static final DreamAnimal bestia = new DreamAnimal("animalsearch", 3);
-	public static final DreamSentience cognitio = new DreamSentience("villagesearch", 3);
-	public static final DreamRepair fabrico = new DreamRepair("repair", 3);
-	public static final DreamHuman humanus = new DreamHuman("playersearch", 3);
-	public static final DreamTool instrumentum = new DreamTool("playeritem", 3);
-	public static final DreamDeath mortuus = new DreamDeath("deathsearch", 3);
-	public static final DreamChange permutatio = new DreamChange("changeeffects", 6);
-	public static final DreamPower potentia = new DreamPower("boosteffects", 6);
-	public static final DreamDarkness tenebrae = new DreamDarkness("innsmouthsearch", 3);
-	public static final DreamVoid vacuos = new DreamVoid("voiddream", 0);
-	public static final DreamStillness vinculum = new DreamStillness("keepeffects", 6);
+	public static final DreamEldritch eldritch = new DreamEldritch("alienis", 3);
+	public static final DreamAnimal animal = new DreamAnimal("animalsearch", 3);
+	public static final DreamSentience sentience = new DreamSentience("villagesearch", 3);
+	public static final DreamRepair repair = new DreamRepair("repair", 3);
+	public static final DreamHuman human = new DreamHuman("playersearch", 3);
+	public static final DreamTool tool = new DreamTool("playeritem", 3);
+	public static final DreamDeath death = new DreamDeath("deathsearch", 3);
+	public static final DreamChange change = new DreamChange("changeeffects", 5);
+	public static final DreamPower power = new DreamPower("boosteffects", 6);
+	public static final DreamDarkness darkness= new DreamDarkness("innsmouthsearch", 3);
+	public static final DreamVoid voidd = new DreamVoid("voiddream", 0);
+	public static final DreamStillness stillness = new DreamStillness("keepeffects", 6);
 	
 	public static final DreamLearning learning = new DreamLearning("learning", 6);
 	
-	public static final DreamGround metallum = new DreamGround("groundscanmetal", 3, Blocks.IRON_ORE.getDefaultState(), Blocks.GOLD_ORE.getDefaultState());
-	public static final DreamGround vitreus = new DreamGround("groundscancrystal", 3, Blocks.DIAMOND_ORE.getDefaultState(), Blocks.EMERALD_ORE.getDefaultState());
+	public static final DreamGround metal = new DreamGround("groundscanmetal", 3, Blocks.IRON_ORE.getDefaultState(), Blocks.GOLD_ORE.getDefaultState());
+	public static final DreamGround crystal = new DreamGround("groundscancrystal", 3, Blocks.DIAMOND_ORE.getDefaultState(), Blocks.EMERALD_ORE.getDefaultState());
 	
 	
 	public static final Map<Memory, Dream> dreams= new EnumMap<>(Memory.class);
 	
 	static {
-		dreams.put(Memory.ELDRITCH, alienis);
-		dreams.put(Memory.ANIMAL, bestia);
-		dreams.put(Memory.SENTIENCE, cognitio);
-		dreams.put(Memory.REPAIR, fabrico);
-		dreams.put(Memory.HUMAN, humanus);
-		dreams.put(Memory.TOOL, instrumentum);
-		dreams.put(Memory.DEATH, mortuus);
-		dreams.put(Memory.CHANGE, permutatio);
-		dreams.put(Memory.POWER, potentia);
-		dreams.put(Memory.DARKNESS, tenebrae);
-		dreams.put(Memory.VOID, vacuos);
-		dreams.put(Memory.STILLNESS, vinculum);
-		dreams.put(Memory.METAL, metallum);
-		dreams.put(Memory.CRYSTAL, vitreus);
+		dreams.put(Memory.ELDRITCH, eldritch);
+		dreams.put(Memory.ANIMAL, animal);
+		dreams.put(Memory.SENTIENCE, sentience);
+		dreams.put(Memory.REPAIR, repair);
+		dreams.put(Memory.HUMAN, human);
+		dreams.put(Memory.TOOL, tool);
+		dreams.put(Memory.DEATH, death);
+		dreams.put(Memory.CHANGE, change);
+		dreams.put(Memory.POWER, power);
+		dreams.put(Memory.DARKNESS, darkness);
+		dreams.put(Memory.VOID, voidd);
+		dreams.put(Memory.STILLNESS, stillness);
+		dreams.put(Memory.METAL, metal);
+		dreams.put(Memory.CRYSTAL, crystal);
 		dreams.put(Memory.LEARNING, learning);
 	}
 	
