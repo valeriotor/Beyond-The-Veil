d36536b5ed2b7b9b76abc8396bb61dcd5fdb7183
Unknown <valeriotorsiello@libero.it>
2020-02-29 00:10:42 +0100

    Added model to Vijhiss and tweaked functioning

--- src/main/java/com/valeriotor/BTV/blocks/flora/PlantVijhiss.java
+++ src/main/java/com/valeriotor/BTV/blocks/flora/PlantVijhiss.java
@@ -1,30 +1,39 @@
 package com.valeriotor.BTV.blocks.flora;
 
+import java.util.List;
 import java.util.Random;
 
+import com.valeriotor.BTV.blocks.EnumHalf;
 import com.valeriotor.BTV.items.ItemRegistry;
 import com.valeriotor.BTV.tileEntities.TileMutator;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.items.ItemHandlerHelper;
 
 public class PlantVijhiss extends BlockTallPlant implements IMutationCatalyst{
-
+	
+	public static final double a = 0.0625;
+	public static final AxisAlignedBB LOWER_BBOX = new AxisAlignedBB(a*5, 0, a*6, a*9, 1, a*11);
+	public static final AxisAlignedBB UPPER_BBOX = new AxisAlignedBB(a*5, 0, a*6, a*9, a*4, a*11);
+	
 	public PlantVijhiss(String name) {
-		super(Material.PLANTS, name);
+		super(Material.WOOD, name);
 		
 	}
 	
@@ -33,12 +42,12 @@ public class PlantVijhiss extends BlockTallPlant implements IMutationCatalyst{
 			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		if(!worldIn.isRemote)
 		if(hand == EnumHand.MAIN_HAND) {
-			if(playerIn.getHeldItem(hand).getItem() instanceof ItemSeeds) {
+			if(playerIn.getHeldItem(hand).getItem() == Items.GOLD_INGOT) {
 				ItemStack seeds = ItemStack.EMPTY;
 				seeds = ItemRegistry.getRandomSeed(RANDOM, 10);
-				//for(int i = 0; i < 100 && seeds == ItemStack.EMPTY; i++)
-				// seeds = ForgeHooks.getGrassSeed(RANDOM, 0); // Wasn't working as intended. 
-				if(seeds == ItemStack.EMPTY) seeds = new ItemStack(getRandomSeed(RANDOM));
+				for(int i = 0; i < 100 && seeds == ItemStack.EMPTY; i++)
+				 seeds = ForgeHooks.getGrassSeed(RANDOM, 0); // Wasn't working as intended. 
+				if(seeds == ItemStack.EMPTY || seeds.getItem() == Items.WHEAT_SEEDS) seeds = new ItemStack(getRandomSeed(RANDOM));
 				if(seeds != ItemStack.EMPTY) {
 					playerIn.getHeldItem(hand).shrink(1);
 					ItemHandlerHelper.giveItemToPlayer(playerIn, seeds);
@@ -90,5 +99,21 @@ public class PlantVijhiss extends BlockTallPlant implements IMutationCatalyst{
 		return 3;
 	}
 	
+	@Override
+	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox,
+			List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean isActualState) {
+		if(state.getValue(EnumHalf.HALF) == EnumHalf.BOTTOM)
+			super.addCollisionBoxToList(pos, entityBox, collidingBoxes, LOWER_BBOX);
+		else
+			super.addCollisionBoxToList(pos, entityBox, collidingBoxes, UPPER_BBOX);
+	}
+	
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		if(state.getValue(EnumHalf.HALF) == EnumHalf.BOTTOM)
+			return LOWER_BBOX;
+		else
+			return UPPER_BBOX;
+	}
 	
 }
