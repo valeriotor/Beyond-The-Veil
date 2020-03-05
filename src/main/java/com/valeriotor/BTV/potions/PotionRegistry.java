c01947ff3b2449a9b511b2b84c02d4b10eccb83b
Unknown <valeriotorsiello@libero.it>
2020-03-05 21:30:37 +0100

    Added names to Potion Effects

--- src/main/java/com/valeriotor/BTV/potions/PotionRegistry.java
+++ src/main/java/com/valeriotor/BTV/potions/PotionRegistry.java
@@ -1,6 +1,7 @@
 package com.valeriotor.BTV.potions;
 
 import net.minecraft.potion.Potion;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -8,12 +9,14 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class PotionRegistry {
 	
-	public static PotionFolly folly = new PotionFolly(true, 0);
-	public static PotionTerror terror = new PotionTerror(true, 1316095);
+	public static Potion folly = new PotionFolly(true, 0);
+	public static Potion terror = new PotionTerror(true, 1316095).setPotionName("terror");
 	
 	@SubscribeEvent
 	public static void register(RegistryEvent.Register<Potion> event) {
 		event.getRegistry().registerAll(folly, terror);
+		folly.setPotionName(new TextComponentTranslation("effect.folly").getFormattedText());
+		terror.setPotionName(new TextComponentTranslation("effect.terror").getFormattedText());
 	}
 	
 }
