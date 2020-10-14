a7365f763c3d70fb539787efff251ed9d294323c
Unknown <valeriotorsiello@libero.it>
2020-10-14 18:20:40 +0200

    AIRevenge may now accept a Predicate

--- src/main/java/com/valeriotor/beyondtheveil/entities/AI/AIRevenge.java
+++ src/main/java/com/valeriotor/beyondtheveil/entities/AI/AIRevenge.java
@@ -1,5 +1,7 @@
 package com.valeriotor.beyondtheveil.entities.AI;
 
+import java.util.function.Predicate;
+
 import com.valeriotor.beyondtheveil.entities.IPlayerMinion;
 
 import net.minecraft.entity.EntityCreature;
@@ -13,21 +15,29 @@ public class AIRevenge extends EntityAITarget{
 	private EntityLiving ent;
     EntityLivingBase attacker;
     private int timer = 0;
+    private final Predicate<EntityLivingBase> predicate;
 	
 	public AIRevenge(EntityCreature creature) {
+		this(creature, null);
+	}
+	
+	public AIRevenge(EntityCreature creature, Predicate<EntityLivingBase> predicate) {
 		super(creature, false);
 		this.ent = creature;
 		this.setMutexBits(1);
+		this.predicate = predicate;
 	}
 
 	@Override
 	public boolean shouldExecute() {
 		if(this.ent != null) {
 			if(ent.getRevengeTarget() != null) {
-				EntityLivingBase attack = ent.getRevengeTarget();
-				if(!(ent instanceof IPlayerMinion && attack instanceof EntityPlayer && ((EntityPlayer)attack).getPersistentID().equals(((IPlayerMinion)ent).getMasterID()))) {
-					this.attacker = attack;
-					return true;
+				if(predicate == null || predicate.test(ent.getRevengeTarget())) {
+					EntityLivingBase attack = ent.getRevengeTarget();
+					if(!(ent instanceof IPlayerMinion && attack instanceof EntityPlayer && ((EntityPlayer)attack).getPersistentID().equals(((IPlayerMinion)ent).getMasterID()))) {
+						this.attacker = attack;
+						return true;
+					}
 				}
 			}
 		}
@@ -46,6 +56,8 @@ public class AIRevenge extends EntityAITarget{
 		if(this.ent.getRevengeTimer() < 0) {
 			return false;
 		}
+		if(predicate != null && !predicate.test(attacker))
+			return false;
 		return super.shouldContinueExecuting();
 	}
 	
