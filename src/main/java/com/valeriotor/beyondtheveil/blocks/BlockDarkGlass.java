3d12bd45242ef48a5af69a2e1e82dfd33c6ea488
Unknown <valeriotorsiello@libero.it>
2020-12-22 19:17:04 +0100

    Dark Glass is now unbreakable and sounds like glass

--- src/main/java/com/valeriotor/beyondtheveil/blocks/BlockDarkGlass.java
+++ src/main/java/com/valeriotor/beyondtheveil/blocks/BlockDarkGlass.java
@@ -1,5 +1,6 @@
 package com.valeriotor.beyondtheveil.blocks;
 
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
@@ -8,6 +9,9 @@ public class BlockDarkGlass extends ModBlock{
 
 	public BlockDarkGlass(String name) {
 		super(Material.GLASS, name);
+		setBlockUnbreakable();
+        setResistance(6000001.0F);
+		setSoundType(SoundType.GLASS);
 	}
 	
 	@Override
