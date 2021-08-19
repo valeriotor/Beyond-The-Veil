1058eaa1f3816afd70b8a7c9dd3fc982652f1013
Unknown <valeriotorsiello@libero.it>
2021-08-19 22:29:26 +0200

    Added description to Ink Mask

--- src/main/java/com/valeriotor/beyondtheveil/items/ItemInkMask.java
+++ src/main/java/com/valeriotor/beyondtheveil/items/ItemInkMask.java
@@ -4,11 +4,18 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.valeriotor.beyondtheveil.lib.References;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class ItemInkMask extends ItemArmor implements IDeepOneItem{
 
@@ -27,5 +34,12 @@ public class ItemInkMask extends ItemArmor implements IDeepOneItem{
     @Override
     public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot) {
         return HashMultimap.create();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        String info = I18n.format(String.format("lore.%s",  this.getUnlocalizedName().substring(19)));
+        if(!info.startsWith("lore")) tooltip.add(info);
     }
 }
