33b74951eb4c2823f8b1faec34650d0ef780ee78
Valeriotor <valeriotorsiello@libero.it>
2023-05-13 01:09:26 +0200

    Work on Fume Spreaders

--- src/main/java/com/valeriotor/beyondtheveil/block/FumeSpreaderBlock.java
+++ src/main/java/com/valeriotor/beyondtheveil/block/FumeSpreaderBlock.java
@@ -1,10 +1,15 @@
 package com.valeriotor.beyondtheveil.block;
 
+import com.valeriotor.beyondtheveil.Registration;
+import com.valeriotor.beyondtheveil.dreaming.Memory;
 import com.valeriotor.beyondtheveil.tile.FumeSpreaderBE;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -17,7 +22,10 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class FumeSpreaderBlock extends Block implements EntityBlock {
 
@@ -42,11 +50,44 @@ public class FumeSpreaderBlock extends Block implements EntityBlock {
 
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-        if (!level.isClientSide() && hand.ordinal() == 0) {
-            level.setBlock(pos, state.setValue(FULL, !state.getValue(FULL)), 3);
-            return InteractionResult.CONSUME;
+        if (level.isClientSide) {
+            return InteractionResult.SUCCESS;
         }
-        return InteractionResult.SUCCESS;
+        if(player.isShiftKeyDown() && state.getValue(FULL)) {
+            level.setBlock(pos, state.setValue(FULL, false), 3);
+            return InteractionResult.SUCCESS;
+        } else if(!player.isShiftKeyDown()){
+            ItemStack stack = player.getItemInHand(hand);
+            if (!state.getValue(FULL) && stack.getItem() == Registration.ONIRIC_INCENSE.get()) {
+                level.setBlock(pos, state.setValue(FULL, true), 3);
+                stack.shrink(1);
+                return InteractionResult.CONSUME;
+            } else if (stack.getItem() == Registration.MEMORY_PHIAL.get() && state.getValue(FULL)) {
+                CompoundTag tag = stack.getOrCreateTag();
+                BlockEntity entity = level.getBlockEntity(pos);
+                if (entity instanceof FumeSpreaderBE be) {
+                    if (tag.contains("memory")) {
+                        Memory memory = Memory.getMemoryFromDataName(tag.getString("memory"));
+                        be.setStoredMemory(memory);
+                        stack.shrink(1);
+                        return InteractionResult.CONSUME;
+                    } else if (be.getStoredMemory() != null) {
+                        Memory m = be.getStoredMemory();
+                        be.setStoredMemory(null);
+                        stack.shrink(1);
+                        ItemStack returned = new ItemStack(Registration.MEMORY_PHIAL.get());
+                        CompoundTag returnedTag = returned.getOrCreateTag();
+                        returnedTag.putString("memory", m.getDataName());
+                        ItemHandlerHelper.giveItemToPlayer(player, returned);
+                        return InteractionResult.SUCCESS;
+                    }
+                }
+                stack.setTag(null);
+                System.out.println("test");
+            }
+        }
+
+        return InteractionResult.PASS;
     }
 
     @Override
@@ -58,5 +99,20 @@ public class FumeSpreaderBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new FumeSpreaderBE(pPos, pState);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pos, Random pRandom) {
+        BlockEntity entity = pLevel.getBlockEntity(pos);
+        if (entity instanceof FumeSpreaderBE be) {
+            Memory memory = be.getStoredMemory();
+            if (memory != null) {
+                int i = memory.getColor();
+                double d0 = (double)(i >> 16 & 255) / 255.0D;
+                double d1 = (double)(i >> 8 & 255) / 255.0D;
+                double d2 = (double)(i >> 0 & 255) / 255.0D;
+                pLevel.addAlwaysVisibleParticle(ParticleTypes.ENTITY_EFFECT, pos.getX()+0.5D, pos.getY()+0.7D, pos.getZ()+0.5D, d0, d1, d2);
+            }
+        }
     }
 }
--- src/main/java/com/valeriotor/beyondtheveil/tile/FumeSpreaderBE.java
+++ src/main/java/com/valeriotor/beyondtheveil/tile/FumeSpreaderBE.java
@@ -3,9 +3,15 @@ package com.valeriotor.beyondtheveil.tile;
 import com.valeriotor.beyondtheveil.Registration;
 import com.valeriotor.beyondtheveil.dreaming.Memory;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
 public class FumeSpreaderBE extends BlockEntity {
 
@@ -14,6 +20,53 @@ public class FumeSpreaderBE extends BlockEntity {
         super(Registration.FUME_SPREADER_BE.get(), pWorldPosition, pBlockState);
     }
 
+    public Memory getStoredMemory() {
+        return storedMemory;
+    }
+
+    public void setStoredMemory(Memory storedMemory) {
+        this.storedMemory = storedMemory;
+        setChanged();
+        level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 2);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        if(pTag != null)
+            super.load(pTag);
+        storedMemory = pTag != null && pTag.contains("memory") ? Memory.getMemoryFromDataName(pTag.getString("memory")) : null;
+    }
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        if (storedMemory != null) {
+            pTag.putString("memory", storedMemory.getDataName());
+        }
+    }
+
+
+    @Override
+    public void handleUpdateTag(CompoundTag tag) {
+        load(tag); // default behaviour, here just to remind me of that
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        CompoundTag tag = new CompoundTag();
+        saveAdditional(tag);
+        return tag;
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
+        load(pkt.getTag());
+    }
 
 
 
