384ed9c44592b85ae38eccf991770e75901e8b56
Valeriotor <valeriotorsiello@libero.it>
2023-08-07 00:10:31 +0200

    Changed waypoint visualization to use quaternions

--- src/main/java/com/valeriotor/beyondtheveil/client/event/RenderEvents.java
+++ src/main/java/com/valeriotor/beyondtheveil/client/event/RenderEvents.java
@@ -64,42 +64,24 @@ public class RenderEvents {
                 RenderSystem.enableBlend();
                 RenderSystem.setShaderTexture(0, NecronomiconGui.RESEARCH_HIGHLIGHT);
                 GameRenderer gameRenderer = Minecraft.getInstance().gameRenderer;
-                poseStack.pushPose();
-                //poseStack.last().pose().multiply(gameRenderer.getProjectionMatrix(fov));
                 for (ClientData.Waypoint waypoint : ClientData.getInstance().waypoints) {
                     if (reminisceTimePressed < 20) {
                         continue;
                     }
+                    // Note: this does not account for FOV changes such as sprinting, nor effects such as bobbing. It only takes into account the FOV value from the options
                     Vec3 vec3 = gameRenderer.getMainCamera().getPosition();
-                    double d0 = vec3.x();
-                    double d1 = vec3.y();
-                    double d2 = vec3.z();
-                    MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
                     BlockPos pos = waypoint.pos;
-                    pos = new BlockPos(-366, 64, -134);
-
-                    double relX = pos.getX() - d0;
-                    double relZ = pos.getZ() - d2;
-
-                    double v = Math.atan2(-relX, relZ) * 180 / Math.PI;
-                    float yRot = player.getViewYRot(event.getPartialTicks()) % 360;
-                    if (yRot > 180) {
-                        yRot -= 360;
+                    Quaternion q = gameRenderer.getMainCamera().rotation().copy();
+                    q.conj();
+                    Vector3f vector = new Vector3f((float) (pos.getX() - vec3.x), (float) (pos.getY() - vec3.y), (float) (pos.getZ() - vec3.z));
+                    vector.transform(q);
+                    double distanceFromScreen = window.getGuiScaledHeight() / Math.tan((Minecraft.getInstance().options.fov / 2) * Math.PI / 180);
+                    double scaleFactor = distanceFromScreen / vector.z() / 2; // I should have divided the camera angle by two I guess, but idk how to do it so this works too, in this context at least
+                    int x = (int) -(vector.x() * scaleFactor);
+                    int y = (int) -(vector.y() * scaleFactor);
+                    if (vector.z() < 0) {
+                        continue;
                     }
-                    double yaw = (yRot - v) % 360;
-                    if (yaw > 180) {
-                        yaw -= 360;
-                    }
-                    yaw *= -1;
-                    double relY = pos.getY() - d1;
-                    double dist = Math.sqrt(relX * relX + relZ * relZ);
-                    double v1 = -Math.atan2(relY, dist) * 180 / Math.PI;
-                    double pitch = v1 - gameRenderer.getMainCamera().getXRot();
-                    double scaleFactor = window.getGuiScaledHeight() / Math.tan((Minecraft.getInstance().options.fov / 2) * Math.PI / 180);
-                    int y = (int) (window.getGuiScaledHeight() * pitch / 70);
-                    int x = (int) (window.getGuiScaledWidth() * yaw / 102);
-                    x = (int) (Math.tan((yaw / 2) * Math.PI / 180) * scaleFactor);
-                    y = (int) (Math.tan((pitch / 2) * Math.PI / 180) * scaleFactor);
                     RenderSystem.setShaderColor(1,1,1,1);
                     int ticks = player.tickCount % 20;
                     while (ticks < 70) {
@@ -111,32 +93,8 @@ public class RenderEvents {
                         poseStack.popPose();
                         ticks += 20;
                     }
-                    //player.getFieldOfViewModifier()
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "fov: " + String.valueOf(Minecraft.getInstance().options.fov), 10, 10, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "relY: " + String.valueOf(relY), 10, 30, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "relZ: " + String.valueOf(relZ), 10, 50, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "yaw: " + String.valueOf(yaw), 10, 70, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "pitch: " + String.valueOf(pitch), 10, 90, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "x: " + String.valueOf(x), 10, 110, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "y: " + String.valueOf(y), 10, 130, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "camera pitch: " + String.valueOf(gameRenderer.getMainCamera().getXRot()), 10, 150, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "v1: " + String.valueOf(v1), 10, 170, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "nearplane topLeft: " + String.valueOf(gameRenderer.getMainCamera().getNearPlane().getTopLeft()), 10, 190, 0xFFFFFFFF);
-                    //GuiComponent.drawString(poseStack, Minecraft.getInstance().font, "nearplane topRight: " + String.valueOf(gameRenderer.getMainCamera().getNearPlane().getTopRight()), 10, 210, 0xFFFFFFFF);
 
                 }
-                poseStack.popPose();
-
-                //poseStack.pushPose();
-                //innerFill(matrix4f, window.getGuiScaledWidth()/3, window.getGuiScaledHeight()/3, window.getGuiScaledWidth()*2/3, window.getGuiScaledHeight()*2/3, (alpha << 24) | 0xFFFFFF);
-                //poseStack.popPose();
-
-                //for (ClientData.Waypoint waypoint : ClientData.getInstance().waypoints) {
-                //    poseStack.pushPose();
-                //    poseStack.translate(0,0,-1);
-                //    innerFill(matrix4f, window.getWidth()/3, window.getHeight()/3, window.getWidth()*2/3, window.getHeight()*2/3, (alpha << 24) | 0xFFFFFF);
-                //    poseStack.popPose();
-                //}
                 poseStack.popPose();
             }
         }
@@ -146,69 +104,45 @@ public class RenderEvents {
     public static void renderWorldLastEvent(RenderLevelLastEvent event) {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player != null) {
-            for (ClientData.Waypoint waypoint : ClientData.getInstance().waypoints) {
+        }
+    }
+
+    @Deprecated
+    private static void legacyWaypointVisualization(RenderLevelLastEvent event, LocalPlayer player) {
+        for (ClientData.Waypoint waypoint : ClientData.getInstance().waypoints) {
 
 
-                //draw(event.getPoseStack());
-                float pPartialTicks = event.getPartialTick();
-                Vec3 vec3 = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
-                double d0 = vec3.x();
-                double d1 = vec3.y();
-                double d2 = vec3.z();
-                MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
-                event.getPoseStack().pushPose();
-                BlockPos pos = waypoint.pos;
-                pos = new BlockPos(-366, 64, -134);
-                double distanceSqr = vec3.distanceToSqr(pos.getX(), pos.getY(), pos.getZ());
-                Vec3 subtract = new Vec3(pos.getX(), pos.getY(), pos.getZ()).subtract(vec3);
-                if (subtract.x * subtract.x + subtract.z * subtract.z > 256 * 256) {
-                    double ratio = 256 / subtract.length();
-                    subtract = new Vec3(subtract.x * ratio, subtract.y, subtract.z * ratio);
-                    // pos = new BlockPos(pos.getX() / ratio, pos.getY() / ratio, pos.getZ() / ratio);
-                }
-                event.getPoseStack().translate(subtract.x(), pos.getY() - d1, subtract.z());
-                //event.getPoseStack().translate(3, 3, 3);
-                final float beamRadius = 0.5F;
-                float f = (float) Math.floorMod(player.tickCount, 40) + event.getPartialTick();
-                float f1 = 1024 < 0 ? f : -f;
-                float f2 = Mth.frac(f1 * 0.2F - (float) Mth.floor(f1 * 0.1F));
-                float f15 = -1.0F + f2;
-                float f16 = (float) 1024 * 1 * (0.5F / beamRadius) + f15;
-                event.getPoseStack().mulPose(Vector3f.YP.rotationDegrees(f * 2.25F - 45.0F));
-                //renderBeaconBeam(event.getPoseStack(), bufferSource, BEAM_LOCATION, event.getPartialTick(), 1.0F, player.tickCount, 0, 1024, new float[]{1,0,0,1}, 0.2F, 0.25F);
-                renderPart(event.getPoseStack(), bufferSource.getBuffer(RenderType.beaconBeam(BEAM_LOCATION, false)), 1, 0, 1, 1.0F, 0, 300, 0.0F, beamRadius, beamRadius, 0.0F, -beamRadius, 0.0F, 0.0F, -beamRadius, 0.0F, 1.0F, f16, f15);
-                event.getPoseStack().popPose();
-                RenderSystem.clear(256, Minecraft.ON_OSX);
-                bufferSource.endBatch();
-                //renderPart(event.getPoseStack(), bufferSource.getBuffer(RenderType.beaconBeam(BEAM_LOCATION, false)), 1, 1, 1, 1.0F, 0, 100, 0.0F, 0.2F, f7, pGlowRadius, -pGlowRadius, pGlowRadius, pGlowRadius, pGlowRadius, 0.0F, 1.0F, f16, f15);
-
-
-                //float texScale = 1.0F;
-                //int heightOffset = 360;
-                ////float texOffset = -((float)-player.tickCount * 0.2F - Mth.m_14143_((float)-gameTime * 0.1F)) * 0.6F;
-                //float red = 1;
-                //float blue = 1;
-                //float green = 1;
-                //float alpha = 1;
-                //VertexConsumer beamBuffer = bufferSource.getBuffer(RenderType.beaconBeam(BEAM_LOCATION, false));
-                ////float f2 = Mth.lerp(pPartialTicks, player.xBobO, player.xBob);
-                ////float f3 = Mth.lerp(pPartialTicks, player.yBobO, player.yBob);
-//
-                //float f = player.walkDist - player.walkDistO;
-                //float f1 = -(player.walkDist + f * pPartialTicks);
-                //float f2 = Mth.lerp(pPartialTicks, player.oBob, player.bob);
-                //event.getPoseStack().translate((double)-(Mth.sin(f1 * (float)Math.PI) * f2 * 0.5F), (double)-(-Math.abs(Mth.cos(f1 * (float)Math.PI) * f2)), 0.0D);
-                //event.getPoseStack().mulPose(Vector3f.ZP.rotationDegrees(-Mth.sin(f1 * (float)Math.PI) * f2 * 3.0F));
-                //event.getPoseStack().mulPose(Vector3f.XP.rotationDegrees(-Math.abs(Mth.cos(f1 * (float)Math.PI - 0.2F) * f2) * 5.0F));
-//
-                //float V2 = -1.0F + 0;
-                //float innerV1 = 360 * texScale * 0.5F / 2 + V2;
-                //renderPart(event.getPoseStack(), beamBuffer, red, blue, green, alpha, 0, heightOffset, 0.0F, 2, 2, 0.0F, -2, 0.0F, 0.0F, -2, 0.0F, 1.0F, innerV1, V2);
-                //event.getPoseStack().popPose();
-                //Minecraft.getInstance().gameRenderer.resetProjectionMatrix(Minecraft.getInstance().gameRenderer.getProjectionMatrix(fov));
-//
-                ////bufferSource.endLastBatch();
+            //draw(event.getPoseStack());
+            float pPartialTicks = event.getPartialTick();
+            Vec3 vec3 = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+            double d0 = vec3.x();
+            double d1 = vec3.y();
+            double d2 = vec3.z();
+            MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+            event.getPoseStack().pushPose();
+            BlockPos pos = waypoint.pos;
+            pos = new BlockPos(-366, 64, -134);
+            double distanceSqr = vec3.distanceToSqr(pos.getX(), pos.getY(), pos.getZ());
+            Vec3 subtract = new Vec3(pos.getX(), pos.getY(), pos.getZ()).subtract(vec3);
+            if (subtract.x * subtract.x + subtract.z * subtract.z > 256 * 256) {
+                double ratio = 256 / subtract.length();
+                subtract = new Vec3(subtract.x * ratio, subtract.y, subtract.z * ratio);
+                // pos = new BlockPos(pos.getX() / ratio, pos.getY() / ratio, pos.getZ() / ratio);
             }
+            event.getPoseStack().translate(subtract.x(), pos.getY() - d1, subtract.z());
+            //event.getPoseStack().translate(3, 3, 3);
+            final float beamRadius = 0.5F;
+            float f = (float) Math.floorMod(player.tickCount, 40) + event.getPartialTick();
+            float f1 = 1024 < 0 ? f : -f;
+            float f2 = Mth.frac(f1 * 0.2F - (float) Mth.floor(f1 * 0.1F));
+            float f15 = -1.0F + f2;
+            float f16 = (float) 1024 * 1 * (0.5F / beamRadius) + f15;
+            event.getPoseStack().mulPose(Vector3f.YP.rotationDegrees(f * 2.25F - 45.0F));
+            //renderBeaconBeam(event.getPoseStack(), bufferSource, BEAM_LOCATION, event.getPartialTick(), 1.0F, player.tickCount, 0, 1024, new float[]{1,0,0,1}, 0.2F, 0.25F);
+            renderPart(event.getPoseStack(), bufferSource.getBuffer(RenderType.beaconBeam(BEAM_LOCATION, false)), 1, 0, 1, 1.0F, 0, 300, 0.0F, beamRadius, beamRadius, 0.0F, -beamRadius, 0.0F, 0.0F, -beamRadius, 0.0F, 1.0F, f16, f15);
+            event.getPoseStack().popPose();
+            RenderSystem.clear(256, Minecraft.ON_OSX);
+            bufferSource.endBatch();
         }
     }
 
