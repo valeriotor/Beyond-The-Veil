a8c12b0e483aaeba4aafd700a809d15eb5af4068
Valeriotor <valeriotorsiello@libero.it>
2024-03-23 22:50:58 +0100

    Experimenting with rendering currents (WIP, using rain code)

--- src/main/java/com/valeriotor/beyondtheveil/client/event/RenderEvents.java
+++ src/main/java/com/valeriotor/beyondtheveil/client/event/RenderEvents.java
@@ -15,6 +15,7 @@ import com.valeriotor.beyondtheveil.tile.FlaskBE;
 import com.valeriotor.beyondtheveil.tile.FlaskShelfBE;
 import com.valeriotor.beyondtheveil.tile.SurgicalBE;
 import com.valeriotor.beyondtheveil.world.dimension.BTVDimensions;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -28,13 +29,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
@@ -56,11 +60,31 @@ import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fml.common.Mod;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 
 import java.awt.*;
 
+import static net.minecraft.client.renderer.LevelRenderer.getLightColor;
+
 @Mod.EventBusSubscriber(modid = References.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class RenderEvents {
+
+    private static final ResourceLocation RAIN_LOCATION = new ResourceLocation(References.MODID, "textures/environment/current.png");
+    private static final float[] rainSizeX = new float[1024];
+    private static final float[] rainSizeZ = new float[1024];
+
+    static {
+        for(int i = 0; i < 32; ++i) {
+            for(int j = 0; j < 32; ++j) {
+                float f = (float)(j - 16);
+                float f1 = (float)(i - 16);
+                float f2 = Mth.sqrt(f * f + f1 * f1);
+                rainSizeX[i << 5 | j] = -f1 / f2;
+                rainSizeZ[i << 5 | j] = f / f2;
+            }
+        }
+    }
+
 
     //private static volatile double fov = 0;
 //
@@ -76,6 +100,130 @@ public class RenderEvents {
     //        event.getCamera().move(-event.getCamera().getMaxZoom(4F), 0, 0);
     //    }
     //}
+
+    @SubscribeEvent
+    public static void renderCurrent(RenderLevelStageEvent event) {
+        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_WEATHER) {
+            return;
+        }
+        Minecraft mc = Minecraft.getInstance();
+        if (mc == null || true) {
+            return;
+        }
+        float pPartialTick = event.getPartialTick();
+        Camera camera = event.getCamera();
+        double pCamX = camera.getPosition().x();
+        double pCamY = camera.getPosition().y();
+        double pCamZ = camera.getPosition().z();
+        float f = 1.0F;
+        if (!(f <= 0.0F)) {
+            mc.gameRenderer.lightTexture().turnOnLightLayer();
+//            pLightTexture.turnOnLightLayer();
+            Level level = mc.level;
+            int i = Mth.floor(pCamX);
+            int j = Mth.floor(pCamY);
+            int k = Mth.floor(pCamZ);
+            Tesselator tesselator = Tesselator.getInstance();
+            BufferBuilder bufferbuilder = tesselator.getBuilder();
+            RenderSystem.disableCull();
+            RenderSystem.enableBlend();
+            RenderSystem.enableDepthTest();
+            int l = 5;
+            if (Minecraft.useFancyGraphics()) {
+                l = 10;
+            }
+
+            RenderSystem.depthMask(Minecraft.useShaderTransparency());
+            int i1 = -1;
+            float f1 = (float)mc.levelRenderer.getTicks() + pPartialTick;
+            RenderSystem.setShader(GameRenderer::getParticleShader);
+            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+            for(int j1 = k - l; j1 <= k + l; ++j1) {
+                for(int k1 = i - l; k1 <= i + l; ++k1) {
+                    //if (Math.abs(j1 - k) < 1 && Math.abs(k1 - i) < 1) {
+                    //    continue;
+                    //}
+                    //if (j1 - k != 1 || k1 - i != 1) {
+                    //    continue;
+                    //}
+                    int l1 = (j1 - k + 16) * 32 + k1 - i + 16;
+                    double d0 = (double)rainSizeX[l1] * 0.5D;
+                    double d1 = (double)rainSizeZ[l1] * 0.5D;
+                    blockpos$mutableblockpos.set((double)k1, camera.getPosition().y(), (double)j1);
+                    Biome biome = level.getBiome(blockpos$mutableblockpos).value();
+                    if (true) {
+                        int i2 = level.getHeight(Heightmap.Types.MOTION_BLOCKING, k1, j1);
+                        i2 = 1;
+                        int j2 = j - l;
+                        int k2 = j + l;
+                        if (j2 < i2) {
+                            j2 = i2;
+                        }
+
+                        if (k2 < i2) {
+                            k2 = i2;
+                        }
+
+                        int l2 = i2;
+                        if (i2 < j) {
+                            l2 = j;
+                        }
+
+                        if (j2 != k2) {
+                            RandomSource randomsource = RandomSource.create((long)(k1 * k1 * 3121 + k1 * 45238971 ^ j1 * j1 * 418711 + j1 * 13761));
+                            blockpos$mutableblockpos.set(k1, j2, j1);
+                            Biome.Precipitation biome$precipitation = biome.getPrecipitationAt(blockpos$mutableblockpos);
+                            if (true) {
+                                if (i1 != 0) {
+                                    if (i1 >= 0) {
+                                        tesselator.end();
+                                    }
+
+                                    i1 = 0;
+                                    RenderSystem.setShaderTexture(0, RAIN_LOCATION);
+                                    bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+                                }
+
+                                int i3 = mc.levelRenderer.getTicks() + k1 * k1 * 3121 + k1 * 45238971 + j1 * j1 * 418711 + j1 * 13761 & 31;
+                                float f2 = -((float)i3 + pPartialTick) / 32.0F * (3.0F + randomsource.nextFloat());
+                                f2 *= 2F;
+                                double d2 = (double)k1 + 0.5D - pCamX;
+                                double d4 = (double)j1 + 0.5D - pCamZ;
+                                float f3 = (float)Math.sqrt(d2 * d2 + d4 * d4) / (float)l;
+                                float f4 = ((1.0F - f3 * f3) * 0.5F + 0.5F) * f;
+                                blockpos$mutableblockpos.set(k1, l2, j1);
+                                int j3 = getLightColor(level, blockpos$mutableblockpos);
+                                //bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ - d1 + 0.5D).uv(0.0F, (float)j2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ + d1 + 0.5D).uv(1.0F, (float)j2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ + d1 + 0.5D).uv(1.0F, (float)k2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ - d1 + 0.5D).uv(0.0F, (float)k2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+
+                                //bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ - d1 + 0.5D).uv(0.0F, (float)j2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ - d1 + 0.5D).uv(1.0F, (float)j2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ + d1 + 0.5D).uv(1.0F, (float)k2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                //bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ + d1 + 0.5D).uv(0.0F, (float)k2 * 0.25F + f2).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+
+                                bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ /*- d1*/ + 0.5D).uv(0.0F + f2, (float)j2 * 1F).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)k2 - pCamY, (double)j1 - pCamZ /*+ d1*/ + 0.5D).uv(0.25F + f2, (float)j2 * 1F).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                bufferbuilder.vertex((double)k1 - pCamX + d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ /*+ d1*/ + 0.5D).uv(0.25F + f2, (float)k2 * 1F).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                                bufferbuilder.vertex((double)k1 - pCamX - d0 + 0.5D, (double)j2 - pCamY, (double)j1 - pCamZ /*- d1*/ + 0.5D).uv(0.0F + f2, (float)k2 * 1F).color(1.0F, 1.0F, 1.0F, f4).uv2(j3).endVertex();
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (i1 >= 0) {
+                tesselator.end();
+            }
+            RenderSystem.enableCull();
+            RenderSystem.disableBlend();
+            //pLightTexture.turnOffLightLayer();
+            mc.gameRenderer.lightTexture().turnOffLightLayer();
+        }
+
+    }
 
     @SubscribeEvent
     public static void fogEvent(ViewportEvent.RenderFog event) {
