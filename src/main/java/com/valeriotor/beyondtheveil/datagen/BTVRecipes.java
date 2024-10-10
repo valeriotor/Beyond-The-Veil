c14560159eab384afddd6a192b9464bf2dfc76bc
Valeriotor <valeriotorsiello@libero.it>
2024-10-10 20:52:32 +0200

    Adding crafting recipes for items required for first research

--- src/main/java/com/valeriotor/beyondtheveil/datagen/BTVRecipes.java
+++ src/main/java/com/valeriotor/beyondtheveil/datagen/BTVRecipes.java
@@ -1,9 +1,12 @@
 package com.valeriotor.beyondtheveil.datagen;
 
+import com.valeriotor.beyondtheveil.Registration;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.*;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.function.Consumer;
 
@@ -15,5 +18,44 @@ public class BTVRecipes extends RecipeProvider {
 
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Registration.MEMORY_SIEVE.get())
+                .pattern("SWS")
+                .pattern(" S ")
+                .pattern("SSS")
+                .define('S', Blocks.STONE)
+                .define('W', Items.WATER_BUCKET)
+                .group("Beyond the Veil")
+                .unlockedBy("has_tear", has(Registration.MEMORY_SIEVE.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Registration.MEMORY_PHIAL.get(), 3)
+                .pattern(" B ")
+                .pattern("GWG")
+                .pattern(" G ")
+                .define('G', Blocks.GLASS)
+                .define('W', Items.WATER_BUCKET)
+                .define('B', ItemTags.WOODEN_BUTTONS)
+                .group("Beyond the Veil")
+                .unlockedBy("has_tear", has(Registration.MEMORY_PHIAL.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Registration.FUME_SPREADER.get(), 1)
+                .pattern("GOG")
+                .pattern("G G")
+                .pattern("OOO")
+                .define('G', Blocks.GLASS)
+                .define('O', Items.GOLD_INGOT)
+                .group("Beyond the Veil")
+                .unlockedBy("has_tear", has(Registration.FUME_SPREADER.get()))
+                .save(consumer);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Registration.ONIRIC_INCENSE.get())
+                .requires(Items.CLAY_BALL)
+                .requires(Items.FLINT)
+                .requires(Blocks.GRAVEL)
+                .group("Beyond the Veil")
+                .unlockedBy("has_tear", has(Registration.ONIRIC_INCENSE.get()))
+                .save(consumer);
+
     }
 }
