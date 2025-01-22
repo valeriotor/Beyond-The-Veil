88133d7dfba309ffb784e49103e325509f2105e6
Valeriotor <valeriotorsiello@libero.it>
2025-01-22 01:25:25 +0100

    Adding surgery ingredients (starting with shells)

--- src/main/java/com/valeriotor/beyondtheveil/Registration.java
+++ src/main/java/com/valeriotor/beyondtheveil/Registration.java
@@ -295,6 +295,7 @@ public class Registration {
     public static final RegistryObject<Item> TONGS = ITEMS.register("tongs", () -> new SurgeryItem(SurgeryItem.SurgeryItemType.TONGS));
     public static final RegistryObject<Item> SEWING_NEEDLE = ITEMS.register("sewing_needle", () -> new Item(ITEM_PROPERTIES));
     public static final RegistryObject<Item> PLUCKED_EYE = ITEMS.register("plucked_eye", SurgeryIngredient::new);
+    public static final RegistryObject<Item> SHELL = ITEMS.register("shell", SurgeryIngredient::new);
 
     public static final RegistryObject<Item> SEDATIVE_BUCKET = ITEMS.register("sedative_bucket", () -> new BucketItem(Registration.SOURCE_FLUID_SEDATIVE, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
     public static final RegistryObject<Item> SOFTENER_BUCKET = ITEMS.register("softener_bucket", () -> new BucketItem(Registration.SOURCE_FLUID_SOFTENER, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
@@ -471,6 +472,7 @@ public class Registration {
                 output.accept(TONGS.get());
                 output.accept(SEWING_NEEDLE.get());
                 output.accept(PLUCKED_EYE.get());
+                output.accept(SHELL.get());
 
                 output.accept(SEDATIVE_BUCKET.get());
                 output.accept(SOFTENER_BUCKET.get());
