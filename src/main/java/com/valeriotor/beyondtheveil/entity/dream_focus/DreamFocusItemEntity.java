ce8dfdbca00d956516c23de989d45bd138ee6867
Valeriotor <valeriotorsiello@libero.it>
2026-05-30 17:01:34 +0200

    Blackjack dream items now knock out villagers

--- src/main/java/com/valeriotor/beyondtheveil/entity/dream_focus/DreamFocusItemEntity.java
+++ src/main/java/com/valeriotor/beyondtheveil/entity/dream_focus/DreamFocusItemEntity.java
@@ -2,6 +2,7 @@ package com.valeriotor.beyondtheveil.entity.dream_focus;
 
 import com.valeriotor.beyondtheveil.Registration;
 import com.valeriotor.beyondtheveil.client.ClientMethods;
+import com.valeriotor.beyondtheveil.item.BlackjackItem;
 import com.valeriotor.beyondtheveil.tile.DreamFocusBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -12,15 +13,24 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -57,6 +67,7 @@ public class DreamFocusItemEntity extends ItemEntity implements DreamFocusMovabl
             if (!moveToNextPoint(points, pointCounter++)) {
                 toBeRemoved = true;
             }
+            interactWithEntity();
             BlockState inState = level().getBlockState(blockPosition());
             if (inState.getBlock() == Registration.CURTAIN.get() && !checkedCurtainsCache.contains(blockPosition())) {
                 Direction.Axis axis = inState.getValue(HorizontalDirectionalBlock.FACING).getAxis();
@@ -85,6 +96,16 @@ public class DreamFocusItemEntity extends ItemEntity implements DreamFocusMovabl
             }
         } else {
             ClientMethods.colorParticle(DustParticleOptions.REDSTONE, getX(), getY(), getZ(), 0,0,0, entityData.get(COLOR));
+        }
+    }
+
+    private void interactWithEntity() {
+        ItemStack item = getItem();
+        if (item.getItem() == Registration.BLACKJACK.get()) { // TODO add more special behaviours?
+            HitResult hitResult = ProjectileUtil.getHitResultOnMoveVector(this, e -> e instanceof Villager);
+            if (hitResult.getType() == HitResult.Type.ENTITY && ((EntityHitResult)hitResult).getEntity() instanceof Villager villager) {
+                BlackjackItem.knockDownVillager(villager);
+            }
         }
     }
 
